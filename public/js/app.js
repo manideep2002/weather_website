57bc97cb5f0ae4c01f8b119e6c9b8c3b864c5a48
manideep bhupalam <bhupalammanideep@gmail.com>
2023-10-07 13:43:11 +0530

    changed port in the url in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             response_msg.textContent='';
             if(data.error || data.length===0){
